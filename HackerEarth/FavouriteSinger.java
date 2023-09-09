e58ec4b7d8480edb880f3a750066ef17510235e0
shreysheth <shreysheth6487@gmail.com>
2023-09-09 19:21:03 +0530

    xyz

--- HackerEarth/FavouriteSinger.java
+++ HackerEarth/FavouriteSinger.java
@@ -3,25 +3,25 @@ import java.math.BigInteger;
 
 public class FavouriteSinger {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // Read the number of songs (n)
-        BigInteger n = new BigInteger(scanner.nextLine());
-        
-        // Read the entire line of input and split it into integers
-        String[] inputLine = scanner.nextLine().split(" ");
-        
-        // Create an array to store the singers
-        BigInteger[] playlist = new BigInteger[inputLine.length];
-        
-        // Parse the input as BigIntegers and store them in the array
-        for (int i = 0; i < inputLine.length; i++) {
-            playlist[i] = new BigInteger(inputLine[i]);
+        try (Scanner scanner = new Scanner(System.in)) {
+            // Read the number of songs (n)
+            BigInteger n = new BigInteger(scanner.nextLine());
+            
+            // Read the entire line of input and split it into integers
+            String[] inputLine = scanner.nextLine().split(" ");
+            
+            // Create an array to store the singers
+            BigInteger[] playlist = new BigInteger[inputLine.length];
+            
+            // Parse the input as BigIntegers and store them in the array
+            for (int i = 0; i < inputLine.length; i++) {
+                playlist[i] = new BigInteger(inputLine[i]);
+            }
+            
+            // Calculate and print the number of favourite singers
+            BigInteger favouriteSingerCount = countFavouriteSingers(n, playlist);
+            System.out.println(favouriteSingerCount);
         }
-        
-        // Calculate and print the number of favourite singers
-        BigInteger favouriteSingerCount = countFavouriteSingers(n, playlist);
-        System.out.println(favouriteSingerCount);
     }
     
     public static BigInteger countFavouriteSingers(BigInteger n, BigInteger[] playlist) {
--- HackerRank/TimeConversion.java
+++ HackerRank/TimeConversion.java
@@ -5,40 +5,44 @@ import java.util.*;
  */
 public class TimeConversion {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
+        try (Scanner sc = new Scanner(System.in)) {
+            String input = sc.nextLine();
 
-        String meridiem = input.substring(8);
-        String[] initTime = input.substring(0, 8).split(":");
-        String time = "";
-        if (meridiem.toLowerCase().equals("am")) {
-            if (Integer.parseInt(initTime[0]) == 12) {
-                initTime[0] = "00";
+            String meridiem = input.substring(8);
+            String[] initTime = input.substring(0, 8).split(":");
+            String time = "";
+            if (meridiem.toLowerCase().equals("am")) {
+                if (Integer.parseInt(initTime[0]) == 12) {
+                    initTime[0] = "00";
+                    for (String item : initTime) {
+                        time += item + ":";
+                    }
+                    System.out.println(time.substring(0, 8));
+                    return;
+                } else {
+                    for (String item : initTime) {
+                        time += item + ":";
+                    }
+                    System.out.println(time.substring(0, 8));
+                    return;
+                }
+            } else {
+                int x = 0;
+                if (Integer.parseInt(initTime[0]) == 12) {
+                    x = Integer.parseInt(initTime[0]);
+                } else {
+                    x = Integer.parseInt(initTime[0]) + 12;
+                }
+                initTime[0] = Integer.toString(x);
                 for (String item : initTime) {
                     time += item + ":";
                 }
                 System.out.println(time.substring(0, 8));
                 return;
-            } else {
-                for (String item : initTime) {
-                    time += item + ":";
-                }
-                System.out.println(time.substring(0, 8));
-                return;
             }
-        } else {
-            int x = 0;
-            if (Integer.parseInt(initTime[0]) == 12) {
-                x = Integer.parseInt(initTime[0]);
-            } else {
-                x = Integer.parseInt(initTime[0]) + 12;
-            }
-            initTime[0] = Integer.toString(x);
-            for (String item : initTime) {
-                time += item + ":";
-            }
-            System.out.println(time.substring(0, 8));
-            return;
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 }
